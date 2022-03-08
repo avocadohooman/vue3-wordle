13b26076abe0fbe3a6ed56c603986d64be453182
Gerhard Molin <gerhard@minimumbadass.com>
2022-03-08 16:07:21 +0200

    keyboard works

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
+		<simple-keyboard @onKeyPress="handleInput"/>
+	</div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SimpleKeyboard from './components/SimpleKeyboard.vue';
+
+export default defineComponent({
+	components: { SimpleKeyboard, },
+	setup() {
+		const handleInput = (key: string) => {
+			console.log('key', key);
+		}
+
+		return {
+			handleInput,
+		}
+	},
+})
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/SimpleKeyboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="simple-keyboard"></div>
+</template>
+
+<script lang="ts">
+	import Keyboard from "simple-keyboard";
+	import "simple-keyboard/build/css/index.css";
+	import { ref, onMounted, defineComponent } from "vue";
+
+
+	export default defineComponent({
+		props: [],
+		emits: ['onKeyPress'],
+		setup(_props, ctx) {
+			const keyboard = ref();
+			
+			const onKeyPress = (button: string) => {
+				ctx.emit("onKeyPress", button);
+			};
+
+			onMounted(() => {
+				keyboard.value = new Keyboard("simple-keyboard", {
+					layout: {
+					default: [
+						"q w e r t y u i o p",
+						"a s d f g h j k l",
+						"{enter} z x c v b n m {bksp}",
+					],
+					},
+					onKeyPress: onKeyPress,
+				});
+			});
+		}
+	})
+</script>
+
+<style>
+
+</style>
